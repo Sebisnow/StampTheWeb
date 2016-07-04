81209cbd3a6b7ab25120f4b9c41b956e17193c87
Server <ubuntu@ip-172-31-18-66.eu-west-1.compute.internal>
2016-07-04 09:09:50 +0000

    prof database

--- app/pdf/events.json.php
+++ app/pdf/events.json.php
@@ -2,27 +2,11 @@
     "result": [
         {
             "class": "event-success",
-            "end": "1463082759031.476",
-            "id": "165",
-            "start": "1463073759031.476",
-            "title": "Invictus Games: Elizabeth Marks gives gold medal to Papworth Hospital - BBC News",
-            "url": "/comp/165"
-        },
-        {
-            "class": "event-success",
-            "end": "1463555627589.294",
-            "id": "173",
-            "start": "1463546627589.294",
-            "title": "Invictus Games: Elizabeth Marks gives gold medal to Papworth Hospital - BBC News",
-            "url": "/comp/173"
-        },
-        {
-            "class": "event-success",
-            "end": "1467105711013.927",
-            "id": "235",
-            "start": "1467096711013.927",
-            "title": "Brexit: Cameron to face EU leaders after vote to leave - BBC News",
-            "url": "/comp/235"
+            "end": "1467313891964.711",
+            "id": "248",
+            "start": "1467304891964.711",
+            "title": "Mohamed Aturban's Homepage",
+            "url": "/comp/248"
         }
     ],
     "success": "1"
