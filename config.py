65a155674a4bd9bd1a446f49f1b89e45751f865b
Sebisnow <sebastian.2.schneider@uni.kn>
2016-07-07 09:40:33 +0200

    some more corrections from waqars earlier commit 7add680

--- config.py
+++ config.py
@@ -42,13 +42,13 @@ class TestingConfig(Config):
 
 class ProductionConfig(Config):
     SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URL') or \
-        'sqlite:///' + os.path.join(basedir, 'data.sqlite')
+        'sqlite:///' + os.path.join(basedir, 'data-dev.sqlite')
 
 
 config = {
     'development': DevelopmentConfig,
     'testing': TestingConfig,
     'production': ProductionConfig,
-
+    'heroku': DevelopmentConfig,
     'default': DevelopmentConfig
 }
