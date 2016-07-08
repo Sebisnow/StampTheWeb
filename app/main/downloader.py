609eb77ee7f04552513154775634772cddef7466
Sebisnow <sebastian.2.schneider@uni.kn>
2016-07-08 17:44:02 +0200

    check_output needs to know ipfs command

--- app/main/downloader.py
+++ app/main/downloader.py
@@ -276,7 +276,7 @@ def create_html_from_url(html_text, ipfs_hash, url):
                 flash(u'Could not create HTML from ' + url, 'error')
             app.logger.error('Could not create HTML from the: ' + url + '\n' + e.strerror + "\n" +
                              e.filename + "\n" + str(e))
-            app.logger.error(e.characters_written)
+            app.logger.error(e.args)
             return None
         except AttributeError as att:
             if not app.config["TESTING"]:
