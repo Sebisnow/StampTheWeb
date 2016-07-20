66997b09fbe2103ac7a3480171fa80689e29f85a
waqaralidetho <waqaralidetho@hotmail.com>
2016-07-20 20:54:52 +0100

    Bug fix in schedule stamps

--- app/main/views.py
+++ app/main/views.py
@@ -46,7 +46,7 @@ def index():
         already_exist = Post.query.filter(and_(Post.urlSite.like(url_site),
                                                Post.hashVal.like(sha256))).first()
         if already_exist is not None:
-            flash('The URL was already submitted and the content of the website ahs not changed since!')
+            flash('The URL was already submitted and the content of the website has not changed since!')
             post_old = Post.query.get_or_404(already_exist.id)
             return render_template('post.html', posts=[post_old], single=True)
         else:
