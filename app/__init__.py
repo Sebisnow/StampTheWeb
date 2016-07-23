d41be96cd68eac3830161ab8673d7844a05b92dd
Sebisnow <sebastian.2.schneider@uni.kn>
2016-07-23 20:24:15 +0200

    small style corrections in init and up to date requirements

--- app/__init__.py
+++ app/__init__.py
@@ -21,25 +21,25 @@ login_manager = LoginManager()
 login_manager.session_protection = 'strong'
 login_manager.login_iew = 'auth.login'
 
-def clever_function(str,domain):
+
+def clever_function(str, domain):
     #Changes the String to highlight text for html5
     insensitive_domain = re.compile(re.escape(domain), re.IGNORECASE)
     if domain.lower() in str.lower():
-        htmlString = insensitive_domain.sub('<mark>'+domain+'</mark>', str)
+        htmlString = insensitive_domain.sub('<mark>' + domain + '</mark>', str)
         return Markup(htmlString)
     else:
         return Markup(str)
 
 
 def create_app(config_name):
-    app = Flask(__name__,static_folder='pdf') #working
+    app = Flask(__name__, static_folder='pdf')  # working
     #app = Flask(__name__, static_url_path='')
     app.config.from_object(config[config_name])
     config[config_name].init_app(app)
-    app.config['UPLOAD_FOLDER'] = 'pdf/' #working
+    app.config['UPLOAD_FOLDER'] = 'pdf/'  # working
 
-
-    #Setting up Logging
+    # Setting up Logging
     handler = RotatingFileHandler('webStamps.log', maxBytes=10000, backupCount=1)
     handler.setLevel(logging.INFO)
     app.logger.addHandler(handler)
@@ -51,7 +51,6 @@ def create_app(config_name):
     login_manager.init_app(app)
     pagedown.init_app(app)
 
-
     from .main import main as main_blueprint
     app.register_blueprint(main_blueprint)
 
@@ -62,7 +61,3 @@ def create_app(config_name):
     app.jinja_env.add_extension('jinja2.ext.do')
 
     return app
-
-
-
-
