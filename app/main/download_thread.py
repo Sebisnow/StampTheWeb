06c1364c989eeec5fb4959f23584124c90b56d60
Sebisnow <sebastian.2.schneider@uni.kn>
2016-08-02 12:39:09 +0200

    exchanged all mentions of python2 package and also added function in download_thread

--- app/main/download_thread.py
+++ app/main/download_thread.py
@@ -1,16 +1,18 @@
 import threading
 import os
+import re
+import requests
 from selenium import webdriver
 from warcat.model import WARC
 from bs4 import BeautifulSoup
-from app.main import downloader as down
 
 exitFlag = 0
+urlPattern = re.compile('^(https?|ftp)://[^\s/$.?#].[^\s]*$')
 js_path = os.path.abspath(os.path.expanduser("~/") + '/bin/phantomjs/lib/phantom/bin/phantomjs')
 
 
 class DownloadThread(threading.Thread):
-    def __init__(self, thread_id, url, prox):
+    def __init__(self, thread_id, url, prox, base_path='app/pdf/'):
         threading.Thread.__init__(self)
         self.threadID = thread_id
         self.url = url
@@ -19,7 +21,7 @@ class DownloadThread(threading.Thread):
             '--proxy-type=socks5',
         ]
         self.phantom = webdriver.PhantomJS(js_path, service_args=service_args)
-        self.path = down.basePath + "/temporary"
+        self.path = base_path + "/temporary"
         if not os.path.exists(self.path):
             os.mkdir(self.path)
         self.path = self.path + "/" + str(thread_id)
@@ -36,8 +38,26 @@ class DownloadThread(threading.Thread):
         self.phantom.get(self.url)
         with open(self.path + "/page_source.html", "w") as file:
             file.write(self.phantom.page_source)
-        image_files = down.load_images(BeautifulSoup(self.phantom.page_source, "lxml"))
+        image_files = self.load_images(BeautifulSoup(self.phantom.page_source, "lxml"))
         html = self.phantom.page_source
         return html
 
+    def load_images(self, soup):
+        files = list()
+        img_ctr = 0
+        current_directory = os.getcwd()
+        os.chdir(self.path)
+        for img in soup.find_all(['amp-img', 'img']):
+            if urlPattern.match(img['src']):
+                filename = 'img' + str(img_ctr)
+                img_ctr += 1
+                r = requests.get(img['src'], stream=True)
+                if r.status_code == 200:
+                    with open(filename, 'wb') as f:
+                        for chunk in r.iter_content(1024):
+                            f.write(chunk)
+                    img['src'] = filename
+                    files.append(filename)
+        os.chdir(current_directory)
+        return files
 
